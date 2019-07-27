cf41ad55707071a35dd4ba17746613ecd6aabf5a
Adauto Vjr <adautovjr@yahoo.com>
2019-07-26 21:45:16 -0300

    Migration process from older project

---
+++ README.md
@@ -0,0 +1,22 @@
+# ItemsCatalog
+Udacity Project
+
+## Requirements
+
+* python 3
+* pip 3
+* postgresql
+
+## Setup
+
+1. Setup ```.env``` file accordingly with yout postgresql user and password
+2. Run the code below to install all the dependencies
+```
+pip3 install -r requirements.txt
+```
+
+## Usage
+Execute the code below to run the server, then access http://localhost:5000
+```
+python3 app.py
+```
---
+++ models.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import psycopg2
+from psycopg2.extensions import AsIs
+from sqlalchemy import create_engine
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils import database_exists, create_database
+from dotenv import load_dotenv
+
+load_dotenv()
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+    categories = relationship('Category')
+    items = relationship('Item')
+
+    @property
+    def serialize(self):
+
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture
+        }
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    name = Column(String(250), nullable=False)
+    items = relationship('Item')
+
+    @property
+    def serialize(self):
+
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    category = relationship(Category)
+    description = Column(String(250))
+
+    @property
+    def serialize(self):
+
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description
+        }
+
+engine = create_engine('postgresql://'+os.getenv('user') +
+                       ':'+os.getenv('password')+'@localhost:5432/catalog')
+
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+Base.metadata.create_all(engine)
